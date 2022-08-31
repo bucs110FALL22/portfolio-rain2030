e26833f70392dd086a4b8c4a8f9bc67bebe05e28
Rain Yang <>
2022-08-31 21:03:20 +0000

    added "hello Rain"

---
+++ ch01/exercises/simple.py
@@ -0,0 +1,32 @@
+#practice problems
+num1 = 10 * 5
+print("10 x 5 = ",num1)
+
+num2 = 10 ** 2
+print("10^2= ",num2)
+
+num3 = 15 / 10
+print("15/10 = ",num3)
+
+num4 = 15 // 10
+print("15/10(without decimals) = ",num4)
+
+num5 = -15 // 10
+print"-15/10(without decimals) = ",(num5)
+#floor division always round down, I guess?
+num6 = 15 % 10
+print("Reminder of 15/10 = ", num6)
+
+num7 = 10 % 5
+print("Reminder of 15/5 = ",num7)
+
+num8 = 10 % 10
+print("Reminder of 10/10 = ",num8)
+
+num9 = 0 % 10
+print"Reminder of 0/10 = ",(num9)
+
+num10 = 10 / 15
+print("10 / 5 = "num10)
+#now num 10 have an issue of not rounding up the last digit displayed
+
