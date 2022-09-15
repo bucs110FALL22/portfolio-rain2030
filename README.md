6ea80e0716c776593518b0ea98bee211dd5b38e1
Rain Yang <112443547+rain2030@users.noreply.github.com>
2022-09-15 20:35:31 +0000

    ch03 lab is now done and this is my new portfolio

---
+++ README.md
@@ -0,0 +1,4 @@
+# CS110 Portfolio
+Preferred Name: Rain Yang
+Discord username: Rayzak
+REPL url: https://github.com/bucs110FALL22/portfolio-rain2030
--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -1,5 +1,7 @@
 import turtle #1. import modules
 import random
+from random import randint
+import pygame
 
 #Part A
 window = turtle.Screen() # 2.  Create a screen
@@ -20,7 +22,30 @@ leonardo.goto(-100,-20)
 ## 5. Your PART A code goes here
 
 
+
+for turn in range(30):
+  michelangelo.down()
+  leonardo.down()
+  michelangelo.forward(randint(1,5))
+  leonardo.forward(randint(1,5))
+  michelangelo.up()
+  leonardo.up()
+  #-----------------------------------------
 # PART B - complete part B here
+pygame.init()
+ 
+# create the display surface object
+# of specific dimension.
+window = pygame.display.set_mode((600, 600))
+ 
+# Fill the scree with white color
+window.fill((255, 255, 255))
+ 
+# Using draw.rect module of
+# pygame to draw the outlined rectangle
+pygame.draw.rect(window, (0, 0, 255),[100, 100, 400, 100], 2)
+
+pygame.display.update()
 
 
 window.exitonclick()
