bdbad8561435c7e52ca04779938c6ed5e3b1e817
Rain Yang <112443547+rain2030@users.noreply.github.com>
2022-10-14 22:15:50 +0000

    lab 5 - scaling is off

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -1,23 +1,48 @@
+import pygame
+import time
+
+pygame.init()
+
+display = pygame.display.set_mode((500,500))
+display.fill("white")
+max_val = 0
+max_so_far = 0
 
 def threenaddone(n):
     counter = 0
-    while n != 1: 
-      print(n,"count:",counter)
+    while n != 1:
+      #print(n,"count:",counter)
       if n % 2 == 0:        # n is even
         n = n // 2
-        counter+=1
+        counter+=1   
       else:                 # n is odd
         n = n * 3 + 1
-        counter += 1
-    print(n,"count:",counter)                 
-threenaddone(101)
+        counter += 1  
+    #print(n,"count:",counter)  
+    return counter
+print("count:",threenaddone(101)) #print(4)
 
 upper_limit = 20
 iters = {}
 start = 2
 
-for start in range (upper_limit):
-  threenaddone(start)
-  iters = iters.append(start)
-  start += 1
-  print(iters)
+for i in range (start, upper_limit+1):
+  count = threenaddone(i)
+  iters[i] = count
+print(iters)
+
+for i in range (start, upper_limit+1):
+  count = threenaddone(i)
+  if count > max_so_far:
+    max_so_far = count
+    max_val = i
+print("max value:",max_val,"max so far:",max_so_far)
+coordinates = list(iters.items())
+pygame.draw.lines(display, 'black', False, coordinates)
+font = pygame.font.SysFont('Arial',15)
+maxsofar = str(max_so_far)
+msg = display.blit(pygame.font.Font.render(font,"Max_so_far:",1,'black'),(300,20))
+msg2 = display.blit(pygame.font.Font.render(font, maxsofar,1,'black'),(430,20))
+pygame.display.flip()
+
+time.sleep(1000)
